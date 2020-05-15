7b7252b051647e525d3938d4f043134c538cd208
Ailln <kinggreenhall@gmail.com>
2020-05-16 01:07:16 +0800

    change socketio url.

--- front/src/components/App.vue
+++ front/src/components/App.vue
@@ -69,8 +69,8 @@ import VueSocketio from 'vue-socket.io'
 import socketio from 'socket.io-client'
 import Vue from 'vue'
 
-Vue.use(VueSocketio, socketio('https://socketio.dovolopor.com'));
-// Vue.use(VueSocketio, socketio('http://127.0.0.1:8002/'))
+// Vue.use(VueSocketio, socketio('https://socketio.dovolopor.com'));
+Vue.use(VueSocketio, socketio('http://127.0.0.1:8002/'))
 
 export default {
   sockets: {
