44796b021a1f0eb3dab1af1d38605c6b50488e8a
Ailln <kinggreenhall@gmail.com>
2020-07-13 14:57:57 +0800

    add cn2an transform.

--- back/server.py
+++ back/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from cn2an import transform
 
 from test import predict
 
@@ -7,20 +8,28 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+answer_dict = {
+    "greet": "你好～",
+    "name": "我叫锅贴。",
+    "age": "喵龄一岁不到～",
+    "sex": "我是公的！",
+    "parents": "Ailln 啊！",
+    "feature": "学猫叫，喵～喵～喵～",
+    "bye": "886"
+}
 
 @socketio.on("receive")
 def recevie_msg(msg):
-    intent = predict(msg)
-    answer_dict = {
-        "greet": "你好～",
-        "name": "我叫锅贴。",
-        "age": "喵龄一岁不到～",
-        "sex": "我是公的！",
-        "parents": "Ailln 啊！",
-        "feature": "学猫叫，喵～喵～喵～",
-        "bye": "886"
-    }
-    answer = answer_dict[intent]
+    if ":" in msg:
+        mode, _msg = msg.split(":")
+        if mode in ["cn2an", "an2cn"]:
+            answer = f"{transform(_msg, mode)} 【by {mode}】"
+        else:
+            intent = predict(msg)
+            answer = answer_dict[intent]
+    else:
+        intent = predict(msg)
+        answer = answer_dict[intent]
 
     with open("./dialog.txt", "a") as f_dialog:
         f_dialog.write(msg+"\t"+answer+"\n")
