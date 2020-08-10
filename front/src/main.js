7b6ba74edaa7af5e4bdb1cc5f7270e0ef9ff7e76
Ailln <kinggreenhall@gmail.com>
2020-08-11 03:10:55 +0800

    update vue-socketio version.

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueSocketio from 'vue-socket.io'
+import VueSocketIO from 'vue-socket.io'
 import socketio from 'socket.io-client'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
@@ -12,7 +12,10 @@ let apiUrl = "http://127.0.0.1:8002/";
 if (process.env.NODE_ENV === "production") {
     apiUrl = "https://socketio.dovolopor.com"
 }
-Vue.use(VueSocketio, socketio(apiUrl))
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: socketio(apiUrl)
+}))
 
 new Vue({
     el: '#app',
